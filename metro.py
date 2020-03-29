eec0ba72905d16ac2273e4fffb626a92ccd9e131
Jeremy Albou <31568300+jalbou@users.noreply.github.com>
2020-03-28 21:23:49 -0400

    Update metro.py

--- metro.py
+++ metro.py
@@ -6,7 +6,7 @@ while True:
 
     payload = {}
     headers = {
-    'Cookie': '__cfduid=d0d97751098823e2f4e391333cb28492e1584373913; METRO_ANONYMOUS_COOKIE=45374623-1053-4a7d-9196-80c333fbefec; hprl=en; hide-store-banner=true; nbVisited=1; NSC_JOqrpj5ubudv2fpeodwdbrdxp2rrpei=ffffffff09023b1c45525d5f4f58455e445a4a423660; sto__session=1585437026467; firstPageAlreadyVisited=false; JSESSIONID=80A09818AE5E25727D8ECDDAB9B0677B; sto__count=1; ADRUM_BTa=R:23|g:e9587403-e4c2-4d35-8b44-58ce106f203b|n:metrorichelieuinc-prod_c22980fa-c09c-4712-b489-98164bef9f11; ADRUM_BT1=R:23|i:268162|e:78'
+    'Cookie': '__cfduid=d0d97751098823e2f4e391333cb28492e1584373913; METRO_ANONYMOUS_COOKIE=45374623-1053-4a7d-9196-80c333fbefec; hprl=en; hide-store-banner=true; nbVisited=1; NSC_JOqrpj5ubudv2fpeodwdbrdxp2rrpei=ffffffff09023b1c45525d5f4f58455e445a4a423660; firstPageAlreadyVisited=false; sto__session=1585444931922; JSESSIONID=D8588216902F07F23A39DCD1ECC98985; sto__count=2'
     }
     response = requests.request("GET", url, headers=headers, data = payload)
     retour = str(response.text.encode('utf8'))
